28f21d599c45fa98f05bc0060a85a86ac921c673
RAQIM FRAZ <79056198+raqimfraz09@users.noreply.github.com>
2025-08-24 16:08:56 +0530

    Add files via upload

---
+++ server/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const cookieParser = require('cookie-parser');
+const userApi = require('./routes/user');
+const CatApi = require('./routes/categories');
+const PodcastApi = require('./routes/podcast');
+
+require('dotenv').config();
+require('./conn/conn'); 
+
+app.use(express.json());
+app.use(cookieParser());
+
+// All Routes 
+app.use("/api/v1", userApi);
+app.use("/api/v1", CatApi);
+app.use("/api/v1", PodcastApi);
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port : ${process.env.PORT}`);
+});
